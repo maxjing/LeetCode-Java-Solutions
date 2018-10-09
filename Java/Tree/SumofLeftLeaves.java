8ab1276ea1a418098561f95b2cb783fae7ada993
maxjing <tvandcc@gmail.com>
2018-10-09 00:49:56 -0700

    s

--- Java/Tree/SumofLeftLeaves.java
+++ Java/Tree/SumofLeftLeaves.java
@@ -35,3 +35,18 @@ public int sumOfLeftLeaves(TreeNode root) {
     }
     return sum;
 }
+
+//iterative
+public int sumOfLeftLeaves(TreeNode root) {
+       if(root == null) return 0;
+    int sum = 0;
+    if(root.left != null){
+        if(root.left.left == null && root.left.right == null){
+            sum += root.left.val;
+        }else{
+            sum += sumOfLeftLeaves(root.left);
+        }
+    }
+    sum += sumOfLeftLeaves(root.right);
+    return sum;
+}
